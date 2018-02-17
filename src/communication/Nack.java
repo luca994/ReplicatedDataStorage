a058147be399c61a3b0bfb73d1f42f568b0af9b5
Luca Santini <luca2.santini@mail.polimi.it>
2018-02-17 18:13:40 +0100

    Added many methods to Reliable channel

--- src/communication/Nack.java
+++ src/communication/Nack.java
@@ -1,7 +1,25 @@
 package communication;
 
 public class Nack extends Event {
+	
+	private Integer targetClock;
+	private Integer targetProcId;
+	
+	public int getTargetProcId() {
+		return (int)targetProcId;
+	}
 
+	public int getTargetClock() {
+		return (int)targetClock;
+	}
+
+	public void setTargetProcId(Integer targetProcId) {
+		this.targetProcId = targetProcId;
+	}
+	
+	public void setTargetClock(Integer targetClock) {
+		this.targetClock = targetClock;
+	}
 	public Nack(int PID, int logClock) {
 		super(PID, logClock);
 	}
